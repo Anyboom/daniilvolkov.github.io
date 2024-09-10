36ec3b09b861fda596c7af99d2945ae2f12f973d
Волков Даниил <danilka.volkov.02@mail.ru>
2024-09-11 00:25:32 +0300

    Изменить BadgeListItem.vue

--- src/components/badge/BadgeListItem.vue
+++ src/components/badge/BadgeListItem.vue
@@ -1,7 +1,5 @@
 <template>
-  <span
-    class="uppercase bg-gray hover:bg-gray-darken py-2 px-5 rounded-lg select-none text-sm"
-  >
+  <span class="uppercase bg-gray py-2 px-5 rounded-lg select-none text-sm">
     <slot></slot>
   </span>
 </template>
