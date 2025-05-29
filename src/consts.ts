219602b03dd13d032105c99d3db03c74d994d874
Даниил <danilka.volkov.02@mail.ru>
2025-05-29 21:44:26 +0300

    Создание нового сайта

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = "Interfaces by Devin";
+export const SITE_DESCRIPTION =
+  "Front-end developer with 10+ years of experience building clean, easy-to-use web and mobile apps. I focus on simple interfaces, thoughtful design, and smooth user experiences.";
+
+export const GITHUB_URL = "https://github.com/devinschulz";
