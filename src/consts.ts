63a5e8655ac8e6dac42c72a2438793d8bb65c5f7
Даниил <danilka.volkov.02@mail.ru>
2025-12-03 00:59:06 +0300

    Обновил

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,4 @@ export const SITE_TITLE = "Даниил Волков";
 export const SITE_DESCRIPTION =
   "Разработчик с более чем 2-летним опытом создания понятных, простых в использовании веб-приложений. Я уделяю особое внимание простым интерфейсам, продуманному дизайну и удобству взаимодействия с пользователями.";
 export const GITHUB_URL = "https://github.com/anyboom";
+export const TELEGRAM_URL = "https://t.me/anyboom";
